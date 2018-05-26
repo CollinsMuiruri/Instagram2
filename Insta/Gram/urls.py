f1edced045a7100c7d37a9772e1902dc474940cb
Collins Muiruri <wanyekicollins@gmail.com>
2018-05-26 12:27:08 +0300

    Update my urls

--- Insta/Gram/urls.py
+++ Insta/Gram/urls.py
@@ -1,17 +1,19 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', views.latest_images, name='latest'),
-    url(r'^search', views.search_results, name='search_results'),
-    url(r'^home/(?P<id>\d+)/$', views.image_detail, name='home'),
-    url(r'^latest/(?P<image_id>\d+)', views.image, name='detail'),
-    url(r'^profile/(?P<profile_id>[-\w]+)/$', views.profile, name='profiles'),
-    url(r'^new/image/$', views.new_image, name='new-image'),
-    url(r'^(?P<id>\d+)/$', views.after_detail, name='after'),
-    url('create/', views.post, name='post'),
+    url('^$', views.home, name='home'),
+    url(r'^profile/', views.profile, name='profile'),
+    url(r'^search/', views.search_results, name='search'),
+    url(r'^upload_image/', views.upload, name='upload'),
+    url(r'^edit/', views.edit, name='edit'),
+    url(r'^settings/', views.settings, name='settings'),
+    url(r'^comment/(?P<pk>\d+)', views.new_comment, name='comment'),
+    url(r'^view_profile/(?P<pk>\d+)', views.view_your_profile, name='yourprofile'),
+    url(r'^like/(?P<operation>.+)/(?P<pk>\d+)', views.like, name='like'),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

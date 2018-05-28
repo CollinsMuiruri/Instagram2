6d56f62e82054f4d5c0679820c885cd0ecd771b6
Collins Muiruri <wanyekicollins@gmail.com>
2018-05-28 07:34:10 +0300

    Major commit changes

--- Insta/Gram/models.py
+++ Insta/Gram/models.py
@@ -5,9 +5,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile(models.Model):
-    profilePic = models.ImageField(upload_to='profile/', null=True, blank=True)
-    bio = models.CharField(max_length=60, blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profilePic = models.ImageField(upload_to='profile/',null=True,blank=True)
+    bio = models.CharField(max_length=60,blank=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.bio
@@ -24,29 +24,30 @@ class Profile(models.Model):
         return profile
 
     @classmethod
-    def find_profile(cls, search_term):
+    def find_profile(cls,search_term):
         profile = cls.objects.filter(user__username__icontains=search_term)
         return profile
 
     @classmethod
-    def update_profile(cls, id, bio):
-        updated = Image.objects.filter(id=id).update(bio=bio)
+    def update_profile(cls,id,bio):
+        updated = Image.objects.filter(id=id).update(bio = bio)
         return updated
 
 
+
+
 class Image(models.Model):
-    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
-    caption = models.CharField(max_length=60)
+    caption = models.CharField(max_length = 60)
+    image = models.ImageField(upload_to='uploads/')
     upload_date = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
     likes = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.caption
-
     class Meta:
-        ordering = ['-upload_date']
+        ordering = ['image']
 
     def save_image(self):
         self.save()
@@ -55,8 +56,8 @@ class Image(models.Model):
         self.delete()
 
     @classmethod
-    def update_caption(cls, id, caption):
-        captioned = Image.objects.filter(id=id).update(caption=caption)
+    def update_caption(cls,id,caption):
+        captioned = Image.objects.filter(id=id).update(caption = caption)
         return captioned
 
     @classmethod
@@ -65,16 +66,15 @@ class Image(models.Model):
         return image
 
     @classmethod
-    def get_image_by_id(cls, id):
+    def get_image_by_id(cls,id):
         image = Image.objects.filter(id=Image.id)
         return image
 
-
 class Comment(models.Model):
-    comments = models.CharField(max_length=60, blank=True, null=True)
+    comments = models.CharField(max_length=60,blank=True,null=True)
     comment_date = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User)
-    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.comments
